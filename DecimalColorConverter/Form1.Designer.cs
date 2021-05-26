d3746647aec1cb3002c00e51ca746e3d39c10ae4
Robert Setter <r.setter@comcast.net>
2021-05-26 03:49:42 -0500

    Add project files.

---
+++ DecimalColorConverter/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace DecimalColorConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.hexTxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.decText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(141, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hex Code";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(141, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 27);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Choose Color";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // hexTxt
+            // 
+            this.hexTxt.Location = new System.Drawing.Point(141, 58);
+            this.hexTxt.Name = "hexTxt";
+            this.hexTxt.ReadOnly = true;
+            this.hexTxt.Size = new System.Drawing.Size(100, 23);
+            this.hexTxt.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(141, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Decimal Literal";
+            // 
+            // decText
+            // 
+            this.decText.Location = new System.Drawing.Point(141, 102);
+            this.decText.Name = "decText";
+            this.decText.ReadOnly = true;
+            this.decText.Size = new System.Drawing.Size(100, 23);
+            this.decText.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 131);
+            this.Controls.Add(this.decText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.hexTxt);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Color Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox hexTxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox decText;
+    }
+}
+
---
+++ DecimalColorConverter/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DecimalColorConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                byte[] myColor = { colorDialog1.Color.R, colorDialog1.Color.G, colorDialog1.Color.B };
+                hexTxt.Text = Convert.ToHexString(myColor);
+                decText.Text = Convert.ToInt32(hexTxt.Text, 16).ToString();
+            }
+        }
+    }
+}
